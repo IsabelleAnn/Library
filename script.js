3e35fc2dd65b895689919fe97b6a59cb2fe3f22d
IsabelleAnn <isabelle.ann85@gmail.com>
2022-07-08 19:51:21 -0500

    Fix Library

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 let myLibrary = [
-    { author: "JRR Tolkien", title: "The Lord of the Rings - The Fellowship of the Ring", pages: "330", status: true, ID: 1 },
-    { author: "JRR Tolkien", title: "The Lord of the Rings - The Two Towers", pages: "400", status: false, ID: 2 },
-    { author: "JRR Tolkien", title: "The Lord of the Rings - Return of the King", pages: "330", status: true, ID: 3 },
-    { author: "JRR Tolkien", title: "The Hobbit", pages: "400", status: false, ID: 4 },
-    { author: "Terry Pratchett", title: "The Color of Magic", pages: "330", status: true, ID: 5 },
-    { author: "Terry Pratchett", title: "Small Gods", pages: "400", status: false, ID: 6 },
-    { author: "Terry Pratchett", title: "The Wee Free Men", pages: "330", status: true, ID: 7 },
-    { author: "Jane Austen", title: "Price and Prejudice", pages: "400", status: false, ID: 8 },
-    { author: "Jane Austen", title: "Emma", pages: "330", status: true, ID: 9 }
+    { author: "JRR Tolkien", title: "The Lord of the Rings - The Fellowship of the Ring", pages: "330", status: true, ID: 1 }, { author: "JRR Tolkien", title: "The Lord of the Rings - The Two Towers", pages: "400", status: false, ID: 2 }, { author: "JRR Tolkien", title: "The Lord of the Rings - Return of the King", pages: "330", status: true, ID: 3 }, { author: "JRR Tolkien", title: "The Hobbit", pages: "400", status: false, ID: 4 }, { author: "Terry Pratchett", title: "The Color of Magic", pages: "330", status: true, ID: 5 }, { author: "Terry Pratchett", title: "Small Gods", pages: "400", status: false, ID: 6 }, { author: "Terry Pratchett", title: "The Wee Free Men", pages: "330", status: true, ID: 7 }, { author: "Jane Austen", title: "Price and Prejudice", pages: "400", status: false, ID: 8 }, { author: "Jane Austen", title: "Emma", pages: "330", status: true, ID: 9 }
 ];
 const mainSection = document.querySelector("#main-container");
 const addBookToLibraryBtn = document.querySelector("#add-book-btn");
